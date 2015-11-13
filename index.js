eb3b5a01e6e40eb6bcb08ff140dbd4fb6ee0ef89
Mark Stratmann <mark@stratmann.me.uk>
2015-11-13 18:14:36 +0000

    Function Complete and working

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var _ = require('underscore');
 var When = require('when');
 
 exports.handler = function(event, context) {
-  console.log(JSON.stringify(event, null, 2));
+  // console.log(JSON.stringify(event, null, 2));
 
   var promises = event.Records.map(function(record){
     return When.promise(function(resolve, reject, notify) {
       // base64 decode, convert to ascii and JSON parse this kinesis record's payload
       var text = new Buffer(record.kinesis.data, 'base64').toString('utf8');
-      console.log(text);
+      // console.log(text);
       var payload = JSON.parse(text);
       console.log(payload);
       var region = record.awsRegion;
